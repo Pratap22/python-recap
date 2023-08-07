25a344a21d214d9756087fee64b71d9f1d131be3
Pratap Sharma <sharma.pratap22@gmail.com>
2023-08-07 09:32:59 +0545

    16

--- 16_functions/code.py
+++ 16_functions/code.py
@@ -1,28 +1,51 @@
-# Functions
-# In programming, a function is a reusable block of code that 
-# executes a certain functionality when it is called
+# # Functions
+# # In programming, a function is a reusable block of code that
+# # executes a certain functionality when it is called
 
-# Types of function
-# 1. Standard library functions
-# 2. User-defined functions
+# # Types of function
+# # 1. Standard library functions
+# # 2. User-defined functions
 
-# def function_name():
-#     # body
-#     print("Learn With Pratap")
+# # def function_name():
+# #     # body
+# #     print("Learn With Pratap")
 
-# function_name()
+# # function_name()
 
-# def sum():
-#     a = 10
-#     b = 11
+# # def sum():
+# #     a = 10
+# #     b = 11
 
-#     print("Sum of two numbers is ", a + b)
+# #     print("Sum of two numbers is ", a + b)
 
-# sum()
+# # sum()
 
-def add_numbers(num1, num2=20):
-    print(num1, num2)
+# def add_numbers(num1, num2=20):
+#     print(num1, num2)
+#     sum = num1 + num2
+#     print("Sum is ", sum)
+
+# add_numbers(19)
+
+def add_numbers(num1: int, num2: int) -> int:
     sum = num1 + num2
-    print("Sum is ", sum)
+    return sum
 
-add_numbers(19)
+
+total = add_numbers(19, 20)
+print(total)
+
+
+def find_square(num):
+    return num * num
+
+
+my_numbers = [1, 2, 3, 4]
+
+sqaure_of_numbers = [find_square(num) for num in my_numbers]
+
+print(sqaure_of_numbers)
+
+# benifits
+# 1. COde reuseable
+# 2. code readability
